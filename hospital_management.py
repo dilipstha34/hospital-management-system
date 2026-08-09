049a0180e6c8dcf3eca5e5e02487dfea103fdcd0
Dilip <dstha0304@gmail.com>
2026-08-09 23:54:54 +0545

    implement login authentication system

--- hospital_management.py
+++ hospital_management.py
@@ -3,6 +3,7 @@ import time
 import datetime
 from tkinter import *
 from tkinter import ttk
+from tkinter import font
 import tkinter.messagebox
 
 
@@ -17,15 +18,18 @@ class windows1:
         self.master = master
         self.master.title("Hospital Management System")
         self.master.geometry("1350x750+0+0")
-
         self.frame = Frame(self.master)
         self.frame.pack()
+
+
+        self.Username = StringVar()
+        self.Password = StringVar()
 
         self.LabelTitle = Label(self.frame, text="Hospital Management System", font=("Arial", 40, "bold"), bd=10, relief="sunken")
         self.LabelTitle.grid(row=0, column=0, columnspan=2, pady=20)
 
         self.LoginFrame1 = Frame(self.frame, bd=10, width=1000, height=300, relief="groove")
-        self.LoginFrame1.grid(row=1, column=0, pady=10)
+        self.LoginFrame1.grid(row=1, column=0)
 
         self.LoginFrame2 = Frame(self.frame, bd=10, width=1000, height=300, relief="groove")
         self.LoginFrame2.grid(row=2, column=0, pady=15)
@@ -33,17 +37,67 @@ class windows1:
         self.LoginFrame3 = Frame(self.frame, width=1000, height=200, bd=10, relief="groove")
         self.LoginFrame3.grid(row=3, column=0, pady=5)
 
-        self.button_reg = Button(self.LoginFrame3, text="Patient Registration Window", font=("arial", 15, "bold"), command=self.Registration_window)
+        self.button_reg = Button(self.LoginFrame3, text="Patient Registration Window", state = DISABLED, font=("arial", 15, "bold"), 
+                                 command=self.Registration_window)
         self.button_reg.grid(row=0, column=0, padx=10, pady=10)
 
-        self.button_Hosp = Button(self.LoginFrame3, text="Hospital Management Window", font=("arial", 15, "bold"), command=self.Hospital_window)
+        self.button_Hosp = Button(self.LoginFrame3, text="Hospital Management Window", state = DISABLED, font=("arial", 15, "bold"), 
+                                  command=self.Hospital_window)
         self.button_Hosp.grid(row=0, column=1, padx=10, pady=10)
 
-        self.button_Dr_appt = Button(self.LoginFrame3, text="Doctor Appointment Window", font=("arial", 15, "bold"), command=self.Dr_Appoint_window)
-        self.button_Dr_appt.grid(row=0, column=2, padx=10, pady=10)
+        self.button_Dr_appt = Button(self.LoginFrame3, text="Doctor Appointment Window", state = DISABLED, font=("arial", 15, "bold"), 
+                                     command=self.Dr_Appoint_window)
+        self.button_Dr_appt.grid(row=1, column=0, padx=10, pady=10)
 
-        self.button_med_stock = Button(self.LoginFrame3, text="Medicine Stock Window", font=("arial", 15, "bold"), command=self.Medicine_window)
-        self.button_med_stock.grid(row=0, column=3, padx=10, pady=10)
+        self.button_med_stock = Button(self.LoginFrame3, text="Medicine Stock Window", state = DISABLED, font=("arial", 15, "bold"), 
+                                       command=self.Medicine_window)
+        self.button_med_stock.grid(row=1, column=1, padx=10, pady=10)
+
+        #user name and password frame
+
+        self.LabelUsername = Label(self.LoginFrame1, text="User Name", font=("arial", 20, "bold"), bd=3)
+        self.LabelUsername.grid(row=0, column=0)
+
+        self.textUsername = Entry(self.LoginFrame1, font=("arial", 20, "bold"), bd=3, textvariable = self.Username)
+        self.textUsername.grid(row=0, column=1, padx=40, pady=15)
+
+        self.LabelPassword = Label(self.LoginFrame1, text="Password", font=("arial", 20, "bold"), bd=3)
+        self.LabelPassword.grid(row=1, column=0)
+ 
+        self.textPassword = Entry(self.LoginFrame1, font=("arial", 20, "bold"), bd=3, textvariable = self.Password, show="*")
+        self.textPassword.grid(row=1 , column=1, padx=40, pady=15)
+
+        self.button_login = Button(self.LoginFrame2, text="Login", width=20, font=("arial", 18, "bold"),
+                                   command=self.login_system)
+        self.button_login.grid(row=0, column=0, padx=10, pady=10)
+
+        self.button_Reset = Button(self.LoginFrame2, text="Reset", width=20, font=("arial", 18, "bold"))
+        self.button_Reset.grid(row=0, column=3, padx=10, pady=10)
+
+        self.button_Exit = Button(self.LoginFrame2, text="Exit", width=20, font=("arial", 18, "bold"))
+        self.button_Exit.grid(row=0, column=6, padx=10, pady=10)
+
+    def login_system(self):
+        user = self.Username.get()
+        pswd = self.Password.get()
+
+        if(user == str("admin") and (pswd == str("admin"))):
+            self.button_reg.config(state = NORMAL)
+            self.button_Hosp.config(state = NORMAL)
+            self.button_Dr_appt.config(state = NORMAL)
+            self.button_med_stock.config(state = NORMAL)
+        else:
+            tkinter.messagebox.askyesno ("Pharmacy Management System", "You have entered an invalid user name or password")
+            self.button_reg.config(state = DISABLED)
+            self.button_Hosp.config(state = DISABLED)
+            self.button_Dr_appt.config(state = DISABLED)
+            self.button_med_stock.config(state = DISABLED)
+
+            # if user name or password is incorrect it will be in its disabled state
+            
+            self.Username.set("")
+            self.Password.set("")
+            self.textUsername.focus()
 
     def Registration_window(self):
         self.newWindow = Toplevel(self.master)
@@ -115,4 +169,4 @@ class windows5:
 
 
 if __name__ == "__main__":
-    main()
+    main() 
